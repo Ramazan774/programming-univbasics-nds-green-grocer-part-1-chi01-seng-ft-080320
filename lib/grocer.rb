fa8766e4ebe838b1dd63adf46ac3b44be72d298d
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 22:26:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,6 +14,8 @@ def consolidate_cart(cart)
   new_cart = []
 cart/each do |grocery_item|
   current_item = find_item_by_name_in_collection(grocery_item[:item],new_cart)
+  if current_item
+    
 end
 end
 # Consult README for inputs and outputs
