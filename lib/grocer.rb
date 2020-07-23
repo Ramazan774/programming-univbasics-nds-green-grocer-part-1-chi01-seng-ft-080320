6948c7ea181a722c8d30d80d2bedd6d59e892c00
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 22:25:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,12 +11,15 @@ end
   # Consult README for inputs and outputs
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
+  new_cart = []
+cart/each do |grocery_item|
+  current_item = find_item_by_name_in_collection(grocery_item[:item],new_cart)
+end
+end
+# Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-end
 
 
   
