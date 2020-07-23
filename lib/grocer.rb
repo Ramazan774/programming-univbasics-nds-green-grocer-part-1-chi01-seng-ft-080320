d09896b0c352da1efe77d6d1e81fd08ce9e0f74a
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 22:37:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,11 +12,21 @@ end
 
 def consolidate_cart(cart)
   new_cart = []
-cart/each do |grocery_item|
-  current_item = find_item_by_name_in_collection(grocery_item[:item],new_cart)
+cart.each do |grocery_item|
+  current_item = find_item_by_name_in_collection
+  (grocery_item[:item],new_cart)
   if current_item
-    
+    new_cart.each do |new_cart_item|
+    if new_cart_item[:item] == current_item[:item]
+      new_cart_item[:count]+=1 
+    end
+    end
+  else
+    grocery_item[:count] = 1 
+    new_cart << grocery_item
 end
+end
+new_cart
 end
 # Consult README for inputs and outputs
   #
